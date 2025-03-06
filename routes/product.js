b943a7d90d1c3ca8cf293194edda448d52f87798
Mahendra2611 <stpmpv30@gmail.com>
2025-03-06 19:00:03 +0530

    low stock

--- routes/product.js
+++ routes/product.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { addProduct, updateProduct, deleteProduct, getProducts } from "../controllers/product.js";
+import { addProduct, updateProduct, deleteProduct, getProducts,getLowStockProducts,updateProductQuantities } from "../controllers/product.js";
 import { authenticateOwner } from "../middlewares/authenticate.js";
 
 const router = express.Router();
@@ -8,5 +8,8 @@ router.post("/:shopId/add", addProduct); // Add item
 router.put("/:productId/update",  updateProduct); // Update item
 router.delete("/:productId/delete",  deleteProduct); // Delete item
 router.get("/:shopId/products", getProducts); // Fetch all products of a shop
+router.get("/low-stock/:shopId", getLowStockProducts);
+router.put("/update-quantities", updateProductQuantities);// Update product quantities
+
 
 export default router;
