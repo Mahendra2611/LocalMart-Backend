ccb9180a1f757f81019a5ce037f36d5fa3658308
MAHENDRA PRATAP VERMA <132124880+Mahendra2611@users.noreply.github.com>
2025-04-06 23:26:50 +0530

    Update generateToken.js

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -4,10 +4,13 @@ import jwt from 'jsonwebtoken';
 const generateToken = (res, id,role="customer") => {
   const token = jwt.sign({ id ,role }, process.env.JWT_SECRET_KEY, { expiresIn: '7d' });
 
-  res.cookie('token', token, {
-    httpOnly: true,
-    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
-  });
+ res.cookie('token', token, {
+  httpOnly: true,
+  secure: true,
+  sameSite: 'None',
+  maxAge: 7 * 24 * 60 * 60 * 1000,
+});
+
 
   return token;
 };
